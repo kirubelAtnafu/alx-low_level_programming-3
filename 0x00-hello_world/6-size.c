f95f49144cadb1c35b50d3a9194cb8c427358fc1
jomojay <djdrjay001@gmail.com>
2022-04-08 05:50:44 +0100

    my task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of char data type:  byte(s) %d\n", sizeof(char));
-	printf("Size of int data type:  byte(s) %d\n", sizeof(int));
-	printf("Size of long data type:  byte(s) %d\n", sizeof(long));
-	printf("Size of long long data type:  byte(s) %d\n", sizeof(long long));
-	printf("Size of float data type:  byte(s)%d\n", sizeof(float));
+	printf("Size of char data type: %d byte(s)\n", sizeof(char));
+	printf("Size of int data type: %d byte(s)\n", sizeof(int));
+	printf("Size of long data type: %d byte(s)\n", sizeof(long));
+	printf("Size of long long data type: %d byte(s)\n", sizeof(long long));
+	printf("Size of float data type: %d byte(s)\n", sizeof(float));
 	return (0);
 }
