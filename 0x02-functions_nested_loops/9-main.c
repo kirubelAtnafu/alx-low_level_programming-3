da907af7b8bd1f68d927eb8afcfb0e6170872db9
jomojay <djdrjay001@gmail.com>
2022-04-13 02:07:43 +0100

    My commit for task 9

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the description
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - Check description
+ * Description: It prints 9 times table starting with 0
+ * Return: Nothing.
+ */
+void times_table(void)
+{
+	int row, col, prod;
+
+	for (rom = 0; row <= 9; row++)
+	{
+		_putchar('0');
+
+		for (col = 1; col <= 9; col++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = row * col;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
