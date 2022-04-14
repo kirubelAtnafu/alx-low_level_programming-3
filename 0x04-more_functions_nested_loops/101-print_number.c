61b56e027e37fad5247df59680df35499337d7d2
jomojay <djdrjay001@gmail.com>
2022-04-14 21:31:42 +0100

    My commit for task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,13 +8,16 @@ void print_integer(int m);
  */
 void print_number(int n)
 {
-	if (n == 0)
-		_putchar('0');
-	else if (n < 0)
+	unsigned int num = n;
+
+	if (n < 0)
 	{
 		_putchar('-');
-		print_integer(n * -1);
+		num = -num;
 	}
-	else
-		print_integer(n);
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
