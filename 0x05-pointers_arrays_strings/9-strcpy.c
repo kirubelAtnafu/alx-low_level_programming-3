87a31e5280aa3185ad13f2362846763ccf6dc790
jomojay <djdrjay001@gmail.com>
2022-04-24 11:16:16 +0100

    error corection

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,12 +12,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = -1;
+	int i = 0;
 
-	do {
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
 		i++;
-		dest[i] = src(i);
-	} while (src[i] != '\0');
+	}
+
+	*(dest + i) = '\0';
 
 	return (dest);
 }
