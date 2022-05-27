d84e107e71ce3afb1fcc3f6141609532c99bc2c8
jomojay <djdrjay001@gmail.com>
2022-05-27 10:42:08 +0100

    corrected 9 and 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,33 +1,29 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_listint_safe - A function that prints the elementsin a  list
- * @head: A pointer to listint_t structure
- * Return: The number of nodes. Exits with 98 on failure
+ * print_listint_safe - function to print all integer elements
+ * @head: pointer to integer list
+ *
+ * Return: number of nodes
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t nodes = 0;
-	const listint_t *one = head, *two = head;
+	long int diff;
 
-	if (head == NULL)
-		exit(98);
-
-	while (one && two && two->next && head)
+	while (head)
 	{
-		one = one->next;
-		two = two->next->next;
-		if (one == two)
-		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			exit(98);
-		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
+		diff = head - head->next;
 		nodes++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
 	}
-	head = NULL;
 	return (nodes);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,49 +1,43 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - A function that inserts node at given index
- * @head: A pointer to listint_t structure
- * @idx: The index of the list
- * @n: An integer data for new node
- * Return: The address to new node at specified index, or NULL if it failed
+ * insert_nodeint_at_index - function to insert node at nth position
+ * @head: pointer to the list
+ * @idx: position ofelement
+ * @n: value to insert
+ *
+ * Return: On success address of element, on failure NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 1;
-	listint_t *new_node, *temp;
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *temp;
+	unsigned int i;
 
-	if (head == NULL)
+	if (!head || !new)
 		return (NULL);
-
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	if (*head == NULL)
-	{
-		*head = new_node;
-		new_node->next = NULL;
-		new_node->n = n;
-		return (new_node);
-	}
-
-	if (idx == 0)
-	{
-		new_node->next = *head;
-		new_node->n = n;
-		*head = new_node;
-		return (new_node);
-	}
 
 	temp = *head;
-	while (i < idx)
-	{
-		temp = temp->next;
-		i++;
-	}
+	new->n = n;
 
-	new_node->n = n;
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (new_node);
+	new->next = NULL;
+
+	for (i = 0; i < idx && temp != NULL; i++)
+	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	return (NULL);
 }
